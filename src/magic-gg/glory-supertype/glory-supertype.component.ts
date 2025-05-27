9c9eb9cfd8565c49f21efef6009f570e43ddc278
PeepsCallMeLushi <peepscallmelushi@gmail.com>
2025-05-28 00:43:01 +0100

    Adding Glory Supertype page

---
+++ src/magic-gg/glory-supertype/glory-supertype.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-glory-supertype',
+  imports: [],
+  templateUrl: './glory-supertype.component.html',
+  styleUrl: './glory-supertype.component.scss'
+})
+export class GlorySupertypeComponent {
+
+}
---
+++ src/shared/enum/prod/card-super-types.ts
@@ -0,0 +1,5 @@
+export enum CardSuperTypes {
+	GLORY = '<a href="/MagicGG/glory-supertype">Glory</a>',
+	LEGENDARY = 'Legendary',
+	WORLD = '<a href="/MagicGG/glossary#World_Supertype">World</a>',
+}
